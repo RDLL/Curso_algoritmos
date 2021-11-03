b41547bf7bb7bebe86631d3ca9a597fb9850bde2
RDLL <rdll00@protonmail.com>
2021-11-03 09:20:16 -0600

    Corregido lo de endl

--- Basica/Unidad_1/Operaciones/operaciones.cpp
+++ Basica/Unidad_1/Operaciones/operaciones.cpp
@@ -6,20 +6,20 @@ int main()
 {
     int a,b;
     float c,d;
-    cout << "******" << "Operaciones con enteros" << "******"<< endl;
-    cout << "Dame dos numeros enteros" << endl;
+    cout << "******" << "Operaciones con enteros" << "******"<< '\n';
+    cout << "Dame dos numeros enteros" << '\n';
     cin >> a >> b;
-    cout << "La suma es: " << a+b << endl;
-    cout << "La resta es: " << a-b << endl;
-    cout << "La multiplicacion es: " << a*b << endl;
-    cout << "La division es: " << a/b << endl;
-    cout << "El modulo es: " << a%b << endl;
-    cout << "******" << "Operaciones con flotantes" << "******"<< endl;
-    cout << "Dame dos numeros flotantes" << endl;
+    cout << "La suma es: " << a+b << '\n';
+    cout << "La resta es: " << a-b << '\n';
+    cout << "La multiplicacion es: " << a*b << '\n';
+    cout << "La division es: " << a/b << '\n';
+    cout << "El modulo es: " << a%b << '\n';
+    cout << "******" << "Operaciones con flotantes" << "******"<< '\n';
+    cout << "Dame dos numeros flotantes" << '\n';
     cin >> c >> d;
-    cout << "La suma es: " << c+d << endl;
-    cout << "La resta es: " << c-d << endl;
-    cout << "La multiplicacion es: " << c*b << endl;
+    cout << "La suma es: " << c+d << '\n';
+    cout << "La resta es: " << c-d << '\n';
+    cout << "La multiplicacion es: " << c*b << '\n';
     cout << "La division es: " << c/d << endl;
     return 0;
 }
