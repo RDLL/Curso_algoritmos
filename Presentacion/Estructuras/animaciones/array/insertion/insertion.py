bf8da85a8139ea25dc407b85025b4092252a9ca5
RDLL <rdll00@protonmail.com>
2023-07-14 00:03:45 -0600

    insertion at begin animation

--- Presentacion/Estructuras/animaciones/array/insertion/insertion.py
+++ Presentacion/Estructuras/animaciones/array/insertion/insertion.py
@@ -23,30 +23,5 @@ class CreateArrayValue(Scene):
             self.play(
                 FadeOut(insert_value)
             )
-        
-        # Add the rectangles to the scene
-        # Remove an element
-        #removed_val = rectangles[2]
-        # Move the removed elemento to up
-        #self.play(
-        #    ApplyMethod(removed_val.shift,UP * 2),
-        #)
-        # Remove the element from the scene
-        #self.play(
-        #    FadeOut(removed_val),    
-        #)
-        # regroups the rectangles again
-        #for i in range(3, len(rectangles)): # 3 is the index that follows after the deleted rectangle
-        #    self.play(ApplyMethod(rectangles[i].shift, LEFT))
-        # Now with another element
-        #removed_val = rectangles[1]
-        #self.play(
-        #    ApplyMethod(removed_val.shift,UP * 2),
-        #)
-        #self.play(
-        #    FadeOut(removed_val),    
-        #)
-        #for i in range(3, len(rectangles)): # the same index continues to be applied because it is before the previously eliminated index.
-        #    self.play(ApplyMethod(rectangles[i].shift, LEFT))
-            
+                   
         self.wait()  # Wait for the animation to complete
